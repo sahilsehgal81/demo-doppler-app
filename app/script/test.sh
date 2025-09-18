d6d822874397d2511e6debd41f904d923b7a7378
Sahil Sehgal <sahil.sehgal81@gmail.com>
2025-09-18 23:54:49 +0530

    Update test.sh

--- app/script/test.sh
+++ app/script/test.sh
@@ -3,12 +3,18 @@ sleep 10
 # URL to check
 URL="http://localhost:80"
 
-# Perform the curl request and get HTTP status code
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+# Expected response substring
+EXPECTED_RESPONSE="Hello World from Doppler!"
 
-if [ "$STATUS_CODE" -eq 200 ]; then
-  echo "Health check passed: $URL returned $STATUS_CODE"
+# Perform the curl request and store the response
+RESPONSE=$(curl -s $URL)
+
+# Check if the response contains the expected substring
+echo "$RESPONSE" | grep -q "$EXPECTED_RESPONSE"
+if [ $? -eq 0 ]; then
+  echo "URL check passed: Response contains '$RESPONSE'"
 else
-  echo "Health check failed: $URL returned $STATUS_CODE"
+  # Log alert message to the log file with a timestamp
+  echo "Application Test failed"
   exit 1
 fi
