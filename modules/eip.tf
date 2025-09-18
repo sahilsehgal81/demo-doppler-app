52ef9389ec33e06100d8769903c4ef6cc31029f5
Sahil Sehgal <sahil.sehgal81@gmail.com>
2025-09-18 11:39:24 +0530

    Update eip.tf

--- modules/eip.tf
+++ modules/eip.tf
@@ -1,7 +1,6 @@
 resource "aws_eip" "elastic_ip_frontend" {
   count    = var.eip ? length(aws_instance.frontend) : 0
   instance = aws_instance.frontend[count.index].id
-  vpc      = true
 
   tags = {
     Name = "${var.env}-${var.project}"
