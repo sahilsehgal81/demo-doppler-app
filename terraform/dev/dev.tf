639211854324e447ce3824ea0e83ffe1bff035de
Sahil Sehgal <sahil.sehgal81@gmail.com>
2025-09-18 11:32:16 +0530

    Update dev.tf

--- terraform/dev/dev.tf
+++ terraform/dev/dev.tf
@@ -3,11 +3,11 @@ module "frontend" {
   env                   = "test"
   project               = "demo"
   key_name              = "demo-test"
-  vpc_id                = "vpc-784fcc02"
-  subnet_id             = "subnet-a53c418b"
+  vpc_id                = "vpc-1305707a"
+  subnet_id             = "subnet-10dc1b6b"
   length                = "1"
   instance_type         = "t3.medium"
-  ami                   = "ami-0e001c9271cf7f3b9"
+  ami                   = "ami-02d26659fd82cf299"
   volume_size           = "50"
   volume_type           = "gp3"
   encrypted             = true
@@ -18,5 +18,5 @@ module "frontend" {
   ssh_cidr_block        = ["0.0.0.0/0"]
   web_ports             = [80, 443]
   web_cidr_blocks       = ["0.0.0.0/0"]
-  iam_role              = "ssm_parameter"
+#  iam_role              = "ssm_parameter"
 }
